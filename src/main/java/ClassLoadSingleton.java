6843a334f100d166f079d88faecb9ce94b403eef
MichalKoszalka <koszalkamichal1993@gmail.com>
2017-10-31 22:19:22 +0100

    Class load singleton and benchmark

---
+++ src/main/java/ClassLoadSingleton.java
@@ -0,0 +1,12 @@
+public class ClassLoadSingleton {
+
+    private static ClassLoadSingleton instance = new ClassLoadSingleton();
+
+    protected ClassLoadSingleton() {
+    }
+
+    public static ClassLoadSingleton getInstance() {
+        return instance;
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,87 @@
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		getMultipleTimesInstancesOfSingletonAndPrintResult(100);
-		getMultipleTimesInstancesOfSynchronizedBlockSingletonAndPrintResult(1000);
-		getMultipleTimesInstancesOfSynchronizedMethodSingletonAndPrintResult(1000);
+		getMultipleTimesInstancesOfSynchronizedBlockSingletonAndPrintResult(100);
+        getMultipleTimesInstancesOfSynchronizedMethodSingletonAndPrintResult(100);
+        getMultipleTimesInstancesOfClassLoadSingletonAndPrintResult(100);
+        getMultipleTimesInstancesOfSingletonAndPrintResult(100);
 
 	}
 
-	private static void getMultipleTimesInstancesOfSynchronizedBlockSingletonAndPrintResult(int times) {
-		Set<SynchronizedBlockSingleton> instances = new HashSet<>();
-		long start = System.currentTimeMillis();
-		for(int i = 0;i<times;i++) {
-			Runnable runner = () -> instances.add(SynchronizedBlockSingleton.getInstance());
-			new Thread(runner, "TestThread"+i).start();
-		}
-		long end = System.currentTimeMillis();
+	private static void getMultipleTimesInstancesOfSynchronizedBlockSingletonAndPrintResult(int times) throws InterruptedException {
+		Set<SynchronizedBlockSingleton> instances = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        long start = System.currentTimeMillis();
+        for(int i = 0;i<times;i++) {
+            executorService.execute( () -> {
+                SynchronizedBlockSingleton instance = SynchronizedBlockSingleton.getInstance();
+                instances.add(instance);
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long end = System.currentTimeMillis();
 
 		System.out.println("getting " + times + " times instance of " + SynchronizedBlockSingleton.class + ": " + instances + " in " + (end - start) + " milliseconds");
 	}
 
-	private static void getMultipleTimesInstancesOfSingletonAndPrintResult(int times) {
-		Set<Singleton> instances = new HashSet<>();
-		long start = System.currentTimeMillis();
-		for(int i = 0;i<times;i++) {
-			Runnable runner = () -> instances.add(Singleton.getInstance());
-			new Thread(runner, "TestThread"+i).start();
-		}
-		long end = System.currentTimeMillis();
+	private static void getMultipleTimesInstancesOfSingletonAndPrintResult(int times) throws InterruptedException {
+        Set<Singleton> instances = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        long start = System.currentTimeMillis();
+        for(int i = 0;i<times;i++) {
+            executorService.execute( () -> {
+                Singleton instance = Singleton.getInstance();
+                instances.add(instance);
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long end = System.currentTimeMillis();
 
 		System.out.println("getting " + times + " times instance of " + Singleton.class + ": " + instances + " in " + (end - start) + " milliseconds");
 	}
 
-	private static void getMultipleTimesInstancesOfSynchronizedMethodSingletonAndPrintResult(int times) {
-		Set<SynchronizedMethodSingleton> instances = new HashSet<>();
-		long start = System.currentTimeMillis();
+	private static void getMultipleTimesInstancesOfSynchronizedMethodSingletonAndPrintResult(int times) throws InterruptedException {
+        Set<SynchronizedMethodSingleton> instances = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        long start = System.currentTimeMillis();
 		for(int i = 0;i<times;i++) {
-			Runnable runner = () -> instances.add(SynchronizedMethodSingleton.getInstance());
-			new Thread(runner, "TestThread"+i).start();
-		}
-		long end = System.currentTimeMillis();
+            executorService.execute( () -> {
+                SynchronizedMethodSingleton instance = SynchronizedMethodSingleton.getInstance();
+                instances.add(instance);
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long end = System.currentTimeMillis();
 
 		System.out.println("getting " + times + " times instance of " + SynchronizedMethodSingleton.class + ": " + instances + " in " + (end - start) + " milliseconds");
 	}
+
+    private static void getMultipleTimesInstancesOfClassLoadSingletonAndPrintResult(int times) throws InterruptedException {
+        Set<ClassLoadSingleton> instances = Collections.newSetFromMap(new ConcurrentHashMap<>());
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        long start = System.currentTimeMillis();
+        for(int i = 0;i<times;i++) {
+           executorService.execute( () -> {
+               ClassLoadSingleton instance = ClassLoadSingleton.getInstance();
+                instances.add(instance);
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        long end = System.currentTimeMillis();
+
+        System.out.println("getting " + times + " times instance of " + ClassLoadSingleton.class + ": " + instances + " in " + (end - start) + " milliseconds");
+    }
 
 }
